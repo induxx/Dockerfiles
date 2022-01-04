997ec460ba3f1ad8158f41638be3ef8f8870c810
Thijs De Paepe <thijs.dp@gmail.com>
2022-01-04 14:47:13 +0100

    added jenkins build images

--- production/build/podman/magento2.4/Jenkins_build_image.sh
+++ production/build/podman/magento2.4/Jenkins_build_image.sh
@@ -4,7 +4,13 @@ set -e
 
 # magento patch versions can have breaking changes / updated dependencies
 
-CONTAINER_NAME=magento2.4.3
+# build process is meant for magento 2.4 and above
+# takes about 10 minutes to build
+
+# usage bash production/build/podman/magento2.4/Jenkins_build_image.sh
+
+CONTAINER_NAME=magento2.4.2
+GIT_REPO_BRANCH=magento/project-community-edition:2.4.2 # magento/project-community-edition:2.4.3 or magento/project-enterprise-edition:2.4.3
 
 LOCAL=$(pwd)
 LOCAL=$LOCAL"/$(dirname "$0")"
@@ -19,7 +25,7 @@ init_project()
     --user $(id -u):$(id -g) \
     --volume $PWD/app:/app \
     --volume ~/.composer/auth.json:/tmp/auth.json \
-    composer create-project --ignore-platform-reqs --repository-url=https://repo.magento.com/ magento/project-community-edition /app
+    composer create-project --ignore-platform-reqs --repository-url=https://repo.magento.com/ ${GIT_REPO_BRANCH} /app
 
     cd app
 
@@ -36,7 +42,10 @@ init_project
 
 podman build -t induxx/cloud:${CONTAINER_NAME} .
 
-#podman push $IMAGEID 188.166.109.224:5000/induxx/cloud:${CONTAINER_NAME}  --tls-verify=false
-podman push $IMAGEID registry.induxx.be:5000/induxx/cloud:${CONTAINER_NAME}  --tls-verify=false
+IMAGEID=$(podman images | grep ${CONTAINER_NAME} | awk '{print $3}')
 
+podman push ${IMAGEID} registry.induxx.be:5000/induxx/cloud:${CONTAINER_NAME} --tls-verify=false
+
+# cleanup
+podman rmi ${IMAGEID}
 rm -rf app
